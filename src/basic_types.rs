664838e11840fd301cbe68c96ed608b11a24c95e
jiazhenwei <manout960920@outlook.com>
2023-05-18 23:24:37 +0800

    use binary search to solve divide

--- src/basic_types.rs
+++ src/basic_types.rs
@@ -19,6 +19,54 @@ impl BasicTypes {
         }
         ans
     }
+
+    /*
+    link: https://leetcode.com/problems/divide-two-integers/
+    we can use binary search to search the [0, dividend] to find the answer
+    time complexity O(log(dividend))
+     */    
+    pub fn divide(dividend: i32, divsor: i32) -> i32 {
+        if dividend == divsor {
+            return 1
+        }
+        let is_neg = dividend.is_negative() ^ divsor.is_negative();
+        let (mut left, mut right) = (0i64, dividend as i64);
+        while left < right {
+            let mid = left + ((right+1-left) >> 1);
+            if dividend as i64 >= BasicTypes::simulate_multiple(divsor.abs() as i64, mid) {
+                left = mid;
+            } else {
+                right = mid-1;
+            }
+        }
+        let ans: i64 = match is_neg {
+            true => -(left as i64),
+            false => left as i64,
+        };
+        if ans > (i32::MAX as i64) {
+            return i32::MAX;
+        }
+        if ans < (i32::MIN as i64) {
+            return i32::MIN;
+        }
+        ans as i32
+
+    }
+
+    fn simulate_multiple(x: i64, y: i64) -> i64 {
+        let mut ans = 0;
+        let mut add = x;
+        let mut y = y;
+        while y > 0 {
+            if y & 1 == 1 {
+                ans += add;
+            }
+            y = y >> 1;
+            add += add;
+        }
+        
+        ans
+    }
 }
 
 #[test]
@@ -28,4 +76,12 @@ fn test_reverse() {
     assert_eq!(BasicTypes::reverse(120), 21);
     assert_eq!(BasicTypes::reverse(0), 0);
     assert_eq!(BasicTypes::reverse(900000), 9);
+}
+
+#[test]
+fn test_divide() {
+    assert_eq!(BasicTypes::divide(10, 3), 3);
+    assert_eq!(BasicTypes::divide(7, -3), -2);
+    assert_eq!(BasicTypes::divide(0, 1), 0);
+    assert_eq!(BasicTypes::divide(1, 1), 1);
 }
