a6e42b57869c0477da870d65254e1a1af8e6fd10
jiazhenwei <manout960920@outlook.com>
2023-06-03 23:12:14 +0800

    solve max profit and array_sign

--- src/array.rs
+++ src/array.rs
@@ -29,7 +29,7 @@ impl ArrarySolution {
             nums.reverse();
         }
     }
-    
+
     /*
     link: https://leetcode.com/problems/permutations/
     easily solved by the next_permutation solved before
@@ -37,7 +37,7 @@ impl ArrarySolution {
     pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
         let mut res = vec![];
         let mut prev_nums = nums.clone();
-        
+
         loop {
             let mut nums_copy = prev_nums.clone();
             ArrarySolution::next_permutation(&mut nums_copy);
@@ -45,17 +45,38 @@ impl ArrarySolution {
             prev_nums = nums_copy;
             if prev_nums == nums {
                 break;
-            } 
+            }
         }
 
-        res 
+        res
+    }
+
+    pub fn array_sign(nums: Vec<i32>) -> i32 {
+        nums.iter().map(|x| x.signum()).product()
+    }
+    
+    /*
+    link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+     */
+    pub fn max_profits(prices: Vec<i32>) -> i32 {
+        // rustic way but slower
+        // prices
+        //     .iter()
+        //     .fold((i32::MAX, 0), |(min_price, max_profit), price| {
+        //         (min_price.min(*price), max_profit.max(price - min_price))
+        //     })
+        //     .1
+        let mut max_profit = i32::MIN;
+        let mut min_price = i32::MAX;
+        for price in prices {
+            min_price = min_price.min(price);
+            max_profit = max_profit.max(price - min_price);
+        }
+        max_profit
     }
 }
 
-
-pub fn main() {
-    
-}
+pub fn main() {}
 
 #[test]
 fn test_next_permutation() {
@@ -78,14 +99,38 @@ fn test_permutation() {
     let input = vec![1, 2, 3];
     let mut output = ArrarySolution::permute(input);
     output.sort();
-    let mut expected = vec![vec![1, 2, 3], vec![1, 3, 2], vec![2, 1, 3], vec![2, 3, 1], vec![3, 1, 2], vec![3, 2, 1]];
+    let mut expected = vec![
+        vec![1, 2, 3],
+        vec![1, 3, 2],
+        vec![2, 1, 3],
+        vec![2, 3, 1],
+        vec![3, 1, 2],
+        vec![3, 2, 1],
+    ];
     expected.sort();
     assert_eq!(output, expected);
-    let input = vec![0,1];
+    let input = vec![0, 1];
     let mut output = ArrarySolution::permute(input);
     output.sort();
     let mut expected = vec![vec![0, 1], vec![1, 0]];
     expected.sort();
 
     assert_eq!(output, expected);
+}
+
+#[test]
+fn test_array_sign() {
+    assert_eq!(ArrarySolution::array_sign(vec![-1, -2, -3, -4, 3, 2, 1]), 1);
+    assert_eq!(ArrarySolution::array_sign(vec![1, 5, 0, 2, -3]), 0);
+    assert_eq!(ArrarySolution::array_sign(vec![-1, 1, -1, 1, -1]), -1);
+    assert_eq!(
+        ArrarySolution::array_sign(vec![i32::MAX / 2, i32::MAX / 2, i32::MIN]),
+        -1
+    );
+}
+
+#[test]
+fn test_max_profits() {
+    assert_eq!(ArrarySolution::max_profits(vec![7, 1, 5, 3, 6, 4]), 5);
+    assert_eq!(ArrarySolution::max_profits(vec![7, 6, 4, 3, 1]), 0);
 }
