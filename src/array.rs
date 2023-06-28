34a9bd2a89e414783ed17ade4302efc340acaf1e
jiazhenwei <manout960920@outlook.com>
2023-06-28 22:13:27 +0800

    solve plus one and merge sorted array

--- src/array.rs
+++ src/array.rs
@@ -288,6 +288,25 @@ impl ArrarySolution {
     pub fn length_of_last_word(s: String) -> i32 {
         s.split_ascii_whitespace().last().unwrap_or("").len() as i32
     }
+
+    pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
+        let mut digits = digits;
+        let mut i = digits.len() - 1;
+        loop {
+            if digits[i] == 9 {
+                digits[i] = 0;
+                if i == 0 {
+                    digits.insert(0, 1);
+                    break;
+                }
+                i -= 1;
+            } else {
+                digits[i] += 1;
+                break;
+            }
+        }
+        digits
+    }
 }
 
 pub fn main() {}
@@ -436,4 +455,14 @@ fn test_length_of_last_word() {
     );
     assert_eq!(ArrarySolution::length_of_last_word(" ".to_string()), 0);
     assert_eq!(ArrarySolution::length_of_last_word("a ".to_string()), 1);
+}
+
+#[test]
+fn test_plus_one() {
+    assert_eq!(ArrarySolution::plus_one(vec![1, 2, 3]), vec![1, 2, 4]);
+    assert_eq!(ArrarySolution::plus_one(vec![4, 3, 2, 1]), vec![4, 3, 2, 2]);
+    assert_eq!(ArrarySolution::plus_one(vec![0]), vec![1]);
+    assert_eq!(ArrarySolution::plus_one(vec![9]), vec![1, 0]);
+    assert_eq!(ArrarySolution::plus_one(vec![9, 9]), vec![1, 0, 0]);
+    assert_eq!(ArrarySolution::plus_one(vec![9, 9, 9]), vec![1, 0, 0, 0]);
 }
--- src/sort.rs
+++ src/sort.rs
@@ -11,6 +11,30 @@ impl SortSolution {
         vec.sort_by(|a, b| b.1.cmp(&a.1));
         vec.into_iter().take(k as usize).map(|(num, _)| num).collect()
     }
+
+    pub fn merge_sorted_array(nums1: &mut Vec<i32>, nums2: &mut Vec<i32>,) {
+        let mut i = 0;
+        let mut j = 0;
+        let mut len1 = nums1.len()-nums2.len();
+        let mut len2 = nums2.len();
+        loop {
+            if i >= len1{
+                nums1[i..].clone_from_slice(&nums2[j..]);
+                break;
+            }
+            if j >= nums2.len() {
+                break;
+            }
+            if nums1[i] > nums2[j] {
+                nums1.insert(i, nums2[j]);
+                nums1.truncate(len1+len2);
+                len1 += 1;
+                len2 -= 1;
+                j += 1;
+            }
+            i += 1;
+        }
+    }
 }
 
 pub fn main() {
@@ -20,4 +44,12 @@ pub fn main() {
 #[test]
 fn test_top_k_fequent() { 
     assert_eq!(SortSolution::top_k_fequent(vec![1,1,1,2,2,3], 2), vec![1,2]);
+}
+
+#[test]
+fn test_merge_sorted_array() { 
+    let mut nums1 = vec![1,2,3,0,0,0];
+    let mut nums2 = vec![2,5,6];
+    SortSolution::merge_sorted_array(&mut nums1, &mut nums2);
+    assert_eq!(nums1, vec![1,2,2,3,5,6]);
 }
