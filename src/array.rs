0ecb524ec31170117739a8182edd178958006552
jiazhenwei <manout960920@outlook.com>
2023-07-23 22:14:29 +0800

    solve min depth and can place flowers

--- src/array.rs
+++ src/array.rs
@@ -467,6 +467,23 @@ impl ArraySolution {
         }
         res
     }
+
+    fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
+        let mut flowerbed = flowerbed;
+        let mut i = 0;
+        let mut count = 0;
+        while i < flowerbed.len() {
+            if flowerbed[i] == 0
+                && (i == 0 || flowerbed[i - 1] == 0)
+                && (i == flowerbed.len() - 1 || flowerbed[i + 1] == 0)
+            {
+                flowerbed[i] = 1;
+                count += 1;
+            }
+            i += 1
+        }
+        count >= n
+    }
 }
 
 pub fn main() {}
@@ -688,6 +705,15 @@ mod tests {
             4
         );
         assert_eq!(ArraySolution::longest_consecutive(vec![0, 3, 7, 2, 5, 8, 4, 6, 0, 1]), 9);
+    }
+
+    #[test]
+    fn test_can_place_flowers() {
+        assert_eq!(ArraySolution::can_place_flowers(vec![1, 0, 0, 0, 1], 1), true);
+        assert_eq!(ArraySolution::can_place_flowers(vec![1, 0, 0, 0, 1], 2), false);
+        assert_eq!(ArraySolution::can_place_flowers(vec![1, 0, 0, 0, 0, 1], 2), false);
+        assert_eq!(ArraySolution::can_place_flowers(vec![0, 0, 1, 0, 1], 1), true);
+        assert_eq!(ArraySolution::can_place_flowers(vec![0, 0, 1, 0, 1], 2), false);
     }
 }
 
--- src/tree.rs
+++ src/tree.rs
@@ -1,23 +1,22 @@
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
-  pub val: i32,
-  pub left: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>,
-  pub right: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>,
+    pub val: i32,
+    pub left: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>,
+    pub right: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>,
 }
 
 impl TreeNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    TreeNode {
-      val,
-      left: None,
-      right: None
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
     }
-  }
 }
 
 struct TreeSolution {}
-
 
 impl TreeSolution {
     pub fn max_depth(root: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>) -> i32 {
@@ -30,21 +29,60 @@ impl TreeSolution {
 
         left.max(right) + 1
     }
+    pub fn min_depth(root: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>) -> i32 {
+        if root.is_none() {
+            return 0;
+        }
+        let left = Self::min_depth(root.as_ref().unwrap().borrow().left.clone());
+        let right = Self::min_depth(root.as_ref().unwrap().borrow().right.clone());
+        if left == 0 || right == 0 {
+            return left.max(right) + 1;
+        }
+        left.min(right) + 1
+    }
 }
 
-#[test]
-fn test_max_depth() {
-    let mut root = TreeNode::new(3);
-    let mut left = TreeNode::new(9);
-    let right = TreeNode::new(20);
-    let left_left = TreeNode::new(15);
-    let left_right = TreeNode::new(7);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    left.left = Some(std::rc::Rc::new(std::cell::RefCell::new(left_left)));
-    left.right = Some(std::rc::Rc::new(std::cell::RefCell::new(left_right)));
+    #[test]
+    fn test_max_depth() {
+        let mut root = TreeNode::new(3);
+        let mut left = TreeNode::new(9);
+        let right = TreeNode::new(20);
+        let left_left = TreeNode::new(15);
+        let left_right = TreeNode::new(7);
 
-    root.left = Some(std::rc::Rc::new(std::cell::RefCell::new(left)));
-    root.right = Some(std::rc::Rc::new(std::cell::RefCell::new(right)));
+        left.left = Some(std::rc::Rc::new(std::cell::RefCell::new(left_left)));
+        left.right = Some(std::rc::Rc::new(std::cell::RefCell::new(left_right)));
 
-    assert_eq!(TreeSolution::max_depth(Some(std::rc::Rc::new(std::cell::RefCell::new(root)))), 3);
+        root.left = Some(std::rc::Rc::new(std::cell::RefCell::new(left)));
+        root.right = Some(std::rc::Rc::new(std::cell::RefCell::new(right)));
+
+        assert_eq!(
+            TreeSolution::max_depth(Some(std::rc::Rc::new(std::cell::RefCell::new(root)))),
+            3
+        );
+    }
+
+    #[test]
+    fn test_min_depth() {
+        let mut root = TreeNode::new(3);
+        let mut left = TreeNode::new(9);
+        let right = TreeNode::new(20);
+        let left_left = TreeNode::new(15);
+        let left_right = TreeNode::new(7);
+
+        left.left = Some(std::rc::Rc::new(std::cell::RefCell::new(left_left)));
+        left.right = Some(std::rc::Rc::new(std::cell::RefCell::new(left_right)));
+
+        root.left = Some(std::rc::Rc::new(std::cell::RefCell::new(left)));
+        root.right = Some(std::rc::Rc::new(std::cell::RefCell::new(right)));
+
+        assert_eq!(
+            TreeSolution::min_depth(Some(std::rc::Rc::new(std::cell::RefCell::new(root)))),
+            2
+        );
+    }
 }
