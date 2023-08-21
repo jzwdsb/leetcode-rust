74c5ccccc834c2a30299e281d9390d0ec1666aed
jiazhenwei <manout960920@outlook.com>
2023-08-21 22:59:33 +0800

    solve two sum

--- src/array.rs
+++ src/array.rs
@@ -581,6 +581,24 @@ impl ArraySolution {
         }
         stack
     }
+
+    /*
+    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+     */
+
+    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        let (mut front, mut back) = (0, numbers.len() - 1);
+        while front < back {
+            let sum = numbers[front] + numbers[back];
+            match target.cmp(&sum) {
+                std::cmp::Ordering::Less => back -= 1,
+                std::cmp::Ordering::Equal => break,
+                std::cmp::Ordering::Greater => front += 1,
+            }
+        }
+
+        vec![front as i32 + 1, back as i32 + 1]
+    }
 }
 
 pub fn main() {}
@@ -882,5 +900,12 @@ mod tests {
             ArraySolution::asteroid_collision(vec![-2, -1, 1, 2]),
             vec![]
         );
+    }
+
+    #[test]
+    fn test_two_sum() {
+        assert_eq!(ArraySolution::two_sum(vec![2, 7, 11, 15], 9), vec![1, 2]);
+        assert_eq!(ArraySolution::two_sum(vec![2, 3, 4], 6), vec![1, 3]);
+        assert_eq!(ArraySolution::two_sum(vec![-1, 0], -1), vec![1, 2]);
     }
 }
