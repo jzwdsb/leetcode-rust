d90d4c94b2340953152c2aa53193e0c97f757526
jiazhenwei <manout960920@outlook.com>
2023-09-08 23:45:36 +0800

    implement heap sort

--- src/sort.rs
+++ src/sort.rs
@@ -37,22 +37,62 @@ impl SortSolution {
             i += 1;
         }
     }
+
+    pub fn heap_sort(data: &mut Vec<i32>) {
+        let len = data.len();
+        for i in (0..len / 2).rev() {
+            Self::heapify(data, len, i);
+        }
+        for i in (1..len).rev() {
+            data.swap(0, i);
+            Self::heapify(data, i, 0);
+        }
+    }
+
+    // heapify the input data to a max heap
+    // definition: heapify the subtree rooted at index i
+    // so that the subtree is a max heap
+    fn heapify(data: &mut Vec<i32>, len: usize, i: usize) {
+        let mut largest = i;
+        let left = 2 * i + 1;
+        let right = 2 * i + 2;
+        if left < len && data[left] > data[largest] {
+            largest = left;
+        }
+        if right < len && data[right] > data[largest] {
+            largest = right;
+        }
+        if largest != i {
+            data.swap(i, largest);
+            Self::heapify(data, len, largest);
+        }
+    }
 }
 
-pub fn main() {}
+#[cfg(test)]
+mod test {
+    use super::*;
 
-#[test]
-fn test_top_k_fequent() {
-    assert_eq!(
-        SortSolution::top_k_fequent(vec![1, 1, 1, 2, 2, 3], 2),
-        vec![1, 2]
-    );
-}
+    #[test]
+    fn test_top_k_fequent() {
+        assert_eq!(
+            SortSolution::top_k_fequent(vec![1, 1, 1, 2, 2, 3], 2),
+            vec![1, 2]
+        );
+    }
 
-#[test]
-fn test_merge_sorted_array() {
-    let mut nums1 = vec![1, 2, 3, 0, 0, 0];
-    let mut nums2 = vec![2, 5, 6];
-    SortSolution::merge_sorted_array(&mut nums1, &mut nums2);
-    assert_eq!(nums1, vec![1, 2, 2, 3, 5, 6]);
+    #[test]
+    fn test_merge_sorted_array() {
+        let mut nums1 = vec![1, 2, 3, 0, 0, 0];
+        let mut nums2 = vec![2, 5, 6];
+        SortSolution::merge_sorted_array(&mut nums1, &mut nums2);
+        assert_eq!(nums1, vec![1, 2, 2, 3, 5, 6]);
+    }
+
+    #[test]
+    fn test_heap_sort() {
+        let mut data = vec![4, 10, 3, 5, 1];
+        SortSolution::heap_sort(&mut data);
+        assert_eq!(data, vec![1, 3, 4, 5, 10]);
+    }
 }
