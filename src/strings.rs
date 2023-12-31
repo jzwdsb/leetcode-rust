e25b8d410006b4bcd76aec8a18a89462ecb07930
jiazhenwei <manout960920@outlook.com>
2023-12-31 22:20:33 +0800

    some dp question

--- src/strings.rs
+++ src/strings.rs
@@ -666,6 +666,38 @@ impl StringSolution {
         }
         max as i32
     }
+
+    pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
+        let mut dp = vec![vec![0; text2.len()+1]; text1.len()+1];
+        let text1 = text1.chars().collect::<Vec<char>>();
+        let text2 = text2.chars().collect::<Vec<char>>();
+        for i in 1..=text1.len()  {
+            for j in 1..=text2.len() {
+                if text1[i-1] == text2[j-1] {
+                    dp[i][j] = dp[i-1][j-1]+1;
+                    continue;
+                }
+                dp[i][j] = dp[i-1][j].max(dp[i][j-1]);
+            }
+        }
+        dp[text1.len()][text2.len()]
+    }
+
+    pub fn longest_common_sub_string(text1: String, text2: String) -> i32 {
+        let mut dp = vec![vec![0; text2.len() + 1]; text1.len() + 1];
+        let text1 = text1.chars().collect::<Vec<char>>();
+        let text2 = text2.chars().collect::<Vec<char>>();
+        let mut max = 0;
+        for i in 1..=text1.len() {
+            for j in 1..=text2.len() {
+                if text1[i - 1] == text2[j - 1] {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    max = std::cmp::max(max, dp[i][j]);
+                }
+            }
+        }
+        max
+    }
 }
 
 #[cfg(test)]
@@ -1004,6 +1036,36 @@ mod tests {
         assert_eq!(
             StringSolution::min_partitions("27346209830709182346".to_string()),
             9
+        );
+    }
+
+    #[test]
+    fn test_longest_common_sub_sequence() {
+        assert_eq!(
+            StringSolution::longest_common_subsequence("abcde".to_string(), "ace".to_string()),
+            3
+        );
+        assert_eq!(
+            StringSolution::longest_common_subsequence(
+                "abc".to_string(),
+                "def".to_string()
+            ),
+            0
+        );
+    }
+
+    #[test]
+    fn test_longest_common_sub_string() {
+        assert_eq!(
+            StringSolution::longest_common_sub_string("abcde".to_string(), "ace".to_string()),
+            1
+        );
+        assert_eq!(
+            StringSolution::longest_common_sub_string(
+                "abc".to_string(),
+                "def".to_string()
+            ),
+            0
         );
     }
 }
