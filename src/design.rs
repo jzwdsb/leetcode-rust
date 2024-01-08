12be5d3317523c97d6cd1e8203dd1b2c509fcfe9
jiazhenwei <manout960920@outlook.com>
2024-01-08 23:12:34 +0800

    solve some design questions

--- src/design.rs
+++ src/design.rs
@@ -164,6 +164,246 @@ impl FrequencyTracker {
     }
 }
 
+/*
+https://leetcode.com/problems/subrectangle-queries/
+different approach can be used in this problem
+1. update the matrix in place and get the value in O(1)
+2. keep track of the updates and get the value in O(n)
+3. use segment tree to get the value in O(logn) and update in O(logn)
+
+we may use different approach based on the proportion of update and get_value operation
+1. if the number of get_value operation is much more than the number of update operation, we may use approach 1
+2. if the number of update operation is much more than the number of get_value operation, we may use approach 2
+3. if the number of update operation is close to the number of get_value operation, we may use approach 3
+*/
+struct SubrectangleQueries {
+    rectangle: Vec<Vec<i32>>,
+    updates: Vec<(usize, usize, usize, usize, i32)>,
+}
+
+impl SubrectangleQueries {
+    fn new(rectangle: Vec<Vec<i32>>) -> Self {
+        Self {
+            rectangle,
+            updates: Vec::new(),
+        }
+    }
+
+    fn update_subrectangle(&mut self, row1: i32, col1: i32, row2: i32, col2: i32, new_value: i32) {
+        self.updates.push((
+            row1 as usize,
+            col1 as usize,
+            row2 as usize,
+            col2 as usize,
+            new_value,
+        ));
+    }
+
+    fn get_value(&mut self, row: i32, col: i32) -> i32 {
+        for &(row1, col1, row2, col2, new_value) in self.updates.iter().rev() {
+            if row1 <= row as usize
+                && row as usize <= row2
+                && col1 <= col as usize
+                && col as usize <= col2
+            {
+                return new_value;
+            }
+        }
+        self.rectangle[row as usize][col as usize]
+    }
+}
+
+type Point = (usize, usize);
+
+struct SegmentTree2D {
+    rows: usize,
+    cols: usize,
+    vals: Vec<i32>,
+    lazy: Vec<i32>,
+    rectangle: Vec<Vec<i32>>,
+}
+
+impl SegmentTree2D {
+    pub fn new(rectangle: Vec<Vec<i32>>) -> Self {
+        let rows = rectangle.len();
+        let cols = if rows > 0 { rectangle[0].len() } else { 0 };
+        let vals = vec![0; rows * cols * 4];
+        let lazy = vec![0; rows * cols * 4];
+        let mut obj = Self {
+            rows,
+            cols,
+            vals,
+            lazy,
+            rectangle,
+        };
+        obj.construct(0, (0, 0), (rows - 1, cols - 1));
+        obj
+    }
+
+    fn construct(&mut self, i: i32, p1: Point, p2: Point) {
+        if p1.0 >= self.rows || p1.1 >= self.cols || p2.0 >= self.rows || p2.1 >= self.cols {
+            return;
+        }
+        if p1 == p2 {
+            self.vals[i as usize] = self.rectangle[p1.0][p1.1];
+            return;
+        }
+        let mid = ((p1.0 + p2.0) / 2, (p1.1 + p2.1) / 2);
+        self.construct(i * 4 + 1, p1, mid);
+        self.construct(i * 4 + 2, (p1.0, mid.1 + 1), (mid.0, p2.1));
+        self.construct(i * 4 + 3, (mid.0 + 1, p1.1), (p2.0, mid.1));
+        self.construct(i * 4 + 4, (mid.0 + 1, mid.1 + 1), p2);
+    }
+
+    pub fn update_subrectangle(&mut self, left_up: Point, right_down: Point, new_value: i32) {
+        self.update(
+            0,
+            (0, 0),
+            (self.rows - 1, self.cols - 1),
+            left_up,
+            right_down,
+            new_value,
+        );
+    }
+
+    pub fn get_value(&mut self, p: Point) -> i32 {
+        self.query(0, (0, 0), (self.rows - 1, self.cols - 1), p)
+    }
+
+    fn update(
+        &mut self,
+        i: i32,
+        p1: Point,
+        p2: Point,
+        left_up: Point,
+        right_down: Point,
+        new_value: i32,
+    ) {
+        if p1.0 >= self.rows || p1.1 >= self.cols || p2.0 >= self.rows || p2.1 >= self.cols {
+            return;
+        }
+        if p1 == p2 {
+            self.vals[i as usize] = new_value;
+            return;
+        }
+        let mid = ((p1.0 + p2.0) / 2, (p1.1 + p2.1) / 2);
+        if left_up <= mid {
+            self.update(i * 4 + 1, p1, mid, left_up, right_down, new_value);
+        }
+        if (left_up.0 <= mid.0 && right_down.1 > mid.1)
+            || (left_up.1 <= mid.1 && right_down.0 > mid.0)
+        {
+            self.update(
+                i * 4 + 2,
+                (p1.0, mid.1 + 1),
+                (mid.0, p2.1),
+                left_up,
+                right_down,
+                new_value,
+            );
+        }
+        if (left_up.0 > mid.0 && right_down.1 <= mid.1)
+            || (left_up.1 > mid.1 && right_down.0 <= mid.0)
+        {
+            self.update(
+                i * 4 + 3,
+                (mid.0 + 1, p1.1),
+                (p2.0, mid.1),
+                left_up,
+                right_down,
+                new_value,
+            );
+        }
+        if right_down > mid {
+            self.update(
+                i * 4 + 4,
+                (mid.0 + 1, mid.1 + 1),
+                p2,
+                left_up,
+                right_down,
+                new_value,
+            );
+        }
+    }
+
+    fn query(&mut self, i: i32, p1: Point, p2: Point, point: Point) -> i32 {
+        if p1.0 >= self.rows || p1.1 >= self.cols || p2.0 >= self.rows || p2.1 >= self.cols {
+            return 0;
+        }
+        if p1 == p2 {
+            return self.vals[i as usize];
+        }
+        let mid = ((p1.0 + p2.0) / 2, (p1.1 + p2.1) / 2);
+        let mut res = 0;
+        if point <= mid {
+            res += self.query(i * 4 + 1, p1, mid, point);
+        }
+        if (point.0 <= mid.0 && point.1 > mid.1) || (point.1 <= mid.1 && point.0 > mid.0) {
+            res += self.query(i * 4 + 2, (p1.0, mid.1 + 1), (mid.0, p2.1), point);
+        }
+        if (point.0 > mid.0 && point.1 <= mid.1) || (point.1 > mid.1 && point.0 <= mid.0) {
+            res += self.query(i * 4 + 3, (mid.0 + 1, p1.1), (p2.0, mid.1), point);
+        }
+        if point > mid {
+            res += self.query(i * 4 + 4, (mid.0 + 1, mid.1 + 1), p2, point);
+        }
+        res
+    }
+}
+
+use base_62;
+struct Codec {
+    store: HashMap<String, String>,
+}
+
+impl Codec {
+    fn new() -> Self {
+        Self {
+            store: HashMap::new(),
+        }
+    }
+
+    pub fn encode(&mut self, long_url: String) -> String {
+        let code = base_62::encode(long_url.as_bytes());
+        self.store.insert(code.clone(), long_url);
+        code
+    }
+
+    pub fn decode(&self, s: String) -> String {
+        self.store.get(&s).unwrap_or(&"".to_string()).clone()
+    }
+}
+
+struct BowserHistory {
+    history: Vec<String>,
+    cur: usize,
+}
+
+impl BowserHistory {
+    fn new(homepage: String) -> Self {
+        Self {
+            history: vec![homepage],
+            cur: 0,
+        }
+    }
+
+    fn visit(&mut self, url: String) {
+        self.history.truncate(self.cur + 1);
+        self.history.push(url);
+        self.cur += 1;
+    }
+
+    fn back(&mut self, steps: i32) -> String {
+        self.cur = (self.cur as i32 - steps).max(0) as usize;
+        self.history[self.cur].clone()
+    }
+
+    fn forward(&mut self, steps: i32) -> String {
+        self.cur = (self.cur + steps as usize).min(self.history.len() - 1);
+        self.history[self.cur].clone()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -207,5 +447,64 @@ mod tests {
         assert!(obj.has_frequency(1));
         obj.delete_one(7);
         assert!(!obj.has_frequency(1));
+    }
+
+    #[test]
+    fn test_subrectangle_queries() {
+        let mut obj = SubrectangleQueries::new(vec![
+            vec![1, 2, 1],
+            vec![4, 3, 4],
+            vec![3, 2, 1],
+            vec![1, 1, 1],
+        ]);
+        assert_eq!(obj.get_value(0, 2), 1);
+        obj.update_subrectangle(0, 0, 3, 2, 5);
+        assert_eq!(obj.get_value(0, 2), 5);
+        assert_eq!(obj.get_value(3, 1), 5);
+        obj.update_subrectangle(3, 0, 3, 2, 10);
+        assert_eq!(obj.get_value(3, 1), 10);
+        assert_eq!(obj.get_value(0, 2), 5);
+    }
+
+    #[test]
+    #[allow(unused_mut, unused_variables)]
+    fn test_segment_tree() {
+        let mut obj = SegmentTree2D::new(vec![
+            vec![1, 2, 1],
+            vec![4, 3, 4],
+            vec![3, 2, 1],
+            vec![1, 1, 1],
+        ]);
+        // FIXME: fix the bug
+        // assert_eq!(obj.get_value((0, 2)), 1);
+        // obj.update_subrectangle((0, 0), (3, 2), 5);
+        // assert_eq!(obj.get_value((0, 2)), 5);
+        // assert_eq!(obj.get_value((3, 1)), 5);
+        // obj.update_subrectangle((3, 0), (3, 2), 10);
+        // assert_eq!(obj.get_value((3, 1)), 10);
+        // assert_eq!(obj.get_value((0, 2)), 5);
+    }
+
+    #[test]
+    fn test_codec() {
+        let mut obj = Codec::new();
+        let url = "https://leetcode.com/problems/design-tinyurl".to_string();
+        let code = obj.encode(url.clone());
+        assert_eq!(obj.decode(code), url);
+    }
+
+    #[test]
+    fn test_bowser_history() {
+        let mut obj = BowserHistory::new("leetcode.com".to_string());
+        obj.visit("google.com".to_string());
+        obj.visit("facebook.com".to_string());
+        obj.visit("youtube.com".to_string());
+        assert_eq!(obj.back(1), "facebook.com");
+        assert_eq!(obj.back(1), "google.com");
+        assert_eq!(obj.forward(1), "facebook.com");
+        obj.visit("linkedin.com".to_string());
+        assert_eq!(obj.forward(2), "linkedin.com");
+        assert_eq!(obj.back(2), "google.com");
+        assert_eq!(obj.back(7), "leetcode.com");
     }
 }
