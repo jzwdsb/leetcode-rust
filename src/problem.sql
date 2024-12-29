4e976576956c18e73ebc539c2043b3188b4ec2be
jiazhenwei <manout960920@outlook.com>
2024-12-29 22:31:36 +0800

    feat: solve duplicate email

--- src/problem.sql
+++ src/problem.sql
@@ -62,3 +62,22 @@ select score,
 	   dense_rank() over(order by score desc) as rank
 from Scores
 order by score desc;
+
+
+/*
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+
+Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+*/
+
+SELECT email 
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1;
