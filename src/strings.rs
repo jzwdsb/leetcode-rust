71204dd6a32ad8167ede649464bd4da26f2308ac
jiazhenwei <manout960920@outlook.com>
2025-02-09 23:01:53 +0800

    chore: solve text justification

--- src/strings.rs
+++ src/strings.rs
@@ -1347,6 +1347,54 @@ pub mod string_solution {
         );
     }
 
+    pub fn full_justify(words: Vec<String>, max_width: i32) -> Vec<String> {
+        let mut res = Vec::new();
+        let mut cur = Vec::new();
+        let mut num_of_letters: i32 = 0;
+
+        for word in &words {
+            if word.len() as i32 + cur.len() as i32 + num_of_letters > max_width {
+                for i in 0..(max_width - num_of_letters) {
+                    let idx = i as usize
+                        % (if cur.len() > 1 {
+                            cur.len() - 1
+                        } else {
+                            cur.len()
+                        });
+                    cur[idx] = format!("{} ", cur[idx]);
+                }
+                res.push(cur.join(""));
+                cur.clear();
+                num_of_letters = 0;
+            }
+            cur.push(word.clone());
+            num_of_letters += word.len() as i32;
+        }
+
+        let last_line = cur.join(" ");
+        res.push(format!("{:<width$}", last_line, width = max_width as usize));
+
+        res
+    }
+    #[test]
+    fn test_full_justify() {
+        assert_eq!(
+            full_justify(
+                vec![
+                    "This".to_string(),
+                    "is".to_string(),
+                    "an".to_string(),
+                    "example".to_string(),
+                    "of".to_string(),
+                    "text".to_string(),
+                    "justification.".to_string()
+                ],
+                16
+            ),
+            vec!["This    is    an", "example  of text", "justification.  "]
+        );
+    }
+
     #[test]
     fn test_append_characters() {
         assert_eq!(
