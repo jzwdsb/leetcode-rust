8c4505ab348795bdb87706a13ba51dee6045e2b8
jiazhenwei <manout960920@outlook.com>
2025-02-12 23:27:06 +0800

    solve titile to number

--- src/strings.rs
+++ src/strings.rs
@@ -1395,6 +1395,21 @@ pub mod string_solution {
         );
     }
 
+    pub fn title_to_number(column_title: String) -> i32 {
+        let mut res = 0;
+        for c in column_title.chars() {
+            res = res * 26 + (c as i32 - 'A' as i32 + 1);
+        }
+        res
+    }
+ 
+    #[test]
+    fn test_title_to_number() {
+        assert_eq!(title_to_number("A".to_string()), 1);
+        assert_eq!(title_to_number("AB".to_string()), 28);
+        assert_eq!(title_to_number("ZY".to_string()), 701);
+    }
+
     #[test]
     fn test_append_characters() {
         assert_eq!(
